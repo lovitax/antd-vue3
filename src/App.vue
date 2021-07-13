897f97466679103b73c923970374f9167339bddc
lovitax <lovitax@sina.com>
2021-07-13 23:57:29 +0800

    feat: button

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Button />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import Button from './components/Button.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Button,
+  },
 });
 </script>
 
---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+    <button @click="btnClick(1)">按钮</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Button',
+  props: {
+    btnType: {
+      required: true,
+      type: String,
+    },
+  },
+  setup(props) {
+    console.log('props');
+
+    const btnClick = (num: number, e: Event) => {
+      // console.log(this); // 这个this、、、
+      console.log(arguments);
+      console.log(num);
+      console.log(e);
+    };
+    return {
+      btnClick,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+</style>
